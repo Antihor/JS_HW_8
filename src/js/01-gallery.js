94e29882ebd3d74b16766e427c00d4bea96df7ce
Antihor <66antigor@gmail.com>
2023-03-16 14:03:29 +0200

    ccc

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,29 @@
+import { galleryItems } from './gallery-items.js';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 import SimpleLightbox from 'simplelightbox';
 // Change code below this line
-
 console.log(SimpleLightbox);
+
+const galleryRef = document.querySelector('.gallery');
+const markup = createGallery(galleryItems);
+
+galleryRef.insertAdjacentHTML('afterbegin', markup);
+
+function createGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" title='${description}'/>
+</a>`;
+    })
+    .join('');
+}
+
+let gallery = new SimpleLightbox('.gallery a');
+gallery.on('show.simplelightbox', function () {
+  captionPosition: 'bottom';
+  captionDelay: 250;
+  // captionData: "alt";
+});
