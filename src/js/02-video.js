d43b9232ba8345580dda76997f9ca93b9c16d8ce
Antihor <66antigor@gmail.com>
2023-03-18 10:42:32 +0200

    ddd

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,20 @@
 import Player from '@vimeo/player';
+let throttle = require('lodash.throttle');
 
 console.log(Player);
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+const onPlay = function (data) {
+  // data is an object containing properties specific to that event
+};
+
+player.on('play', onPlay);
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
